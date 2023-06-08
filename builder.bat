874deb08bf2bf834a901715480db8f4300081306
yu34567890 <133307919+yu34567890@users.noreply.github.com>
2023-06-08 20:25:56 +0300

    Update builder.bat

--- builder.bat
+++ builder.bat
@@ -1,6 +1,6 @@
 @echo off
 title Compiling DisFunc
 echo Starting to compile DisFunc
-pyinstaller source.py --uac-admin --onefile --noconsole
+pyinstaller source.py --uac-admin --onefile --noconsole --icon="NONE"
 echo. 
 echo Finished compiling cybergods rat
